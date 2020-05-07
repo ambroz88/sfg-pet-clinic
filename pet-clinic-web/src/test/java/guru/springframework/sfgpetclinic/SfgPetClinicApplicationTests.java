45aac2e7d02c7cfd4f9412963500daf285fb5990
Jiri Ambroz <ambroz88@seznam.cz>
2020-05-07 10:55:50 +0200

    Add web resources and dependencies

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -3,10 +3,10 @@ package guru.springframework.sfgpetclinic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SfgPetClinicApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
